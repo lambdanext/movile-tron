6f3621a018ce90a793e28047a4a5ffd222380da8
Christophe Grand <christophe@cgrand.net>
2015-08-28 14:28:08 -0300

    fix lein disabling opts on windows

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :jvm-opts ^:replace [] ; lein 😒
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [quil "2.0.0"]
                  [clj-http "2.0.0"]
