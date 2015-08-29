6790fe0612d3b400ab67ccf935b62b341610b4dc
Meikel Brandmeyer <mb@kotka.de>
2015-08-29 02:46:10 +0200

    Make smartass java ready

--- src/movile_tron/smartass.clj
+++ src/movile_tron/smartass.clj
@@ -1,17 +1,36 @@
 (ns movile-tron.smartass)
 
+(defn left  [[x y]] [(inc x) y])
+(defn right [[x y]] [(dec x) y])
+(defn up    [[x y]] [x (dec y)])
+(defn down  [[x y]] [x (inc y)])
+
+(defn path-length
+  [direction current-position arena]
+  (->> (iterate direction current-position)
+    next
+    (take-while (fn [p] (not (contains? arena p))))
+    count))
+
 (defn smartass
   [state arena]
-  (let [left  (fn [[x y]] [(inc x) y])
-        right (fn [[x y]] [(dec x) y])
-        up    (fn [[x y]] [x (dec y)])
-        down  (fn [[x y]] [x (inc y)])
-        path-length (fn [direction]
+  (let [best-dir (max-key #(path-length % (:pos state) arena)
+                          left right up down)]
+    ; (assoc state :pos (best-dir (get state :pos)))
+    (update-in state [:pos] best-dir)))
+
+
+
+
+
+
+(defn real-smartass
+  [state arena]
+  (let [path-length (fn [direction]
                       (->> (iterate direction (:pos state))
-                       next
-                       (take-while
-                         (fn [p] (not (contains? arena p))))
-                       count))
-        best-dir    (max-key path-length left right up down)]
+                        next
+                        (take-while (fn [p] (not (contains? arena p))))
+                        count))
+        best-dir (max-key path-length left right up down)]
     ; (assoc state :pos (best-dir (get state :pos)))
     (update-in state [:pos] best-dir)))
